dac3d75106493211a747f8b1e6501d0c2491f7f3
Bruno Barreira <brunobarreirasilva@gmail.com>
2018-11-24 20:46:15 -0200

    Quebra de página - vantagens de comprar na sense finalizada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="<?php echo asset('css/header.css');?>" />
         <link rel="stylesheet" href="<?php echo asset('css/navbar.css');?>" />
         <link rel="stylesheet" href="<?php echo asset('css/banner.css');?>" />
+        <link rel="stylesheet" href="<?php echo asset('css/como-comprar.css');?>" />
         <link rel="stylesheet" href="<?php echo asset('css/vitrine.css');?>" />
         <link href="<?php echo asset('bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" />
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
@@ -21,6 +22,8 @@
         @include('navbar')
 
         @include('banner')
+
+        @include('comoComprar')
 
         @include('vitrine')
          
