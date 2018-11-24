f5ab69c875018062b87b76a41a723e3fdc026119
Bruno Barreira <brunobarreirasilva@gmail.com>
2018-11-24 21:09:34 -0200

    Quebra de página - vantagens de comprar na sense finalizada para o mobile

--- resources/views/comoComprar.blade.php
+++ resources/views/comoComprar.blade.php
@@ -1,28 +1,36 @@
 <section class="container-fluid como-comprar">
     <div class="row">
         <div class="vantagem col-12 col-sm-4 col-lg-4 col-xl-4">
-            <div class="icon">
-                <img src="img/comoComprar/desconto.svg" alt="">
-            </div>
-            <div class="texts">
-                <h3>Desconto de 5%</h3>
-                <small>Nas compras á vista</small>
-            </div>
-        </div>
-        <div class="vantagem col-12 col-sm-4 col-lg-4 col-xl-4">
-            <div class="icon">
-                <img src="img/comoComprar/frete-gratis.svg" alt="">
-            </div>
-            <div class="texts">
-                <h3>Frete Grátis</h3>
-                <small>Para compras acima de R$199,00</small>
+            <div class="centralizar">
+                <div class="icon">
+                    <img src="img/comoComprar/desconto.svg" alt="">
+                </div>
+                <div class="texts">
+                    <h3>Desconto de 5%</h3>
+                    <small>Nas compras á vista</small>
+                </div>
             </div>
         </div>
         <div class="vantagem col-12 col-sm-4 col-lg-4 col-xl-4">
-            <div class="icon"><img src="img/comoComprar/semJuros.svg" alt=""></div>
-            <div class="texts">
-                <h3>Até 6x sem juros</h3>
-                <small>Parcela mínima de R$15,00</small>
+            <div class="centralizar">
+                <div class="icon">
+                    <img src="img/comoComprar/frete-gratis.svg" alt="">
+                </div>
+                <div class="texts">
+                    <h3>Frete Grátis</h3>
+                    <small>Para compras acima de R$199,00</small>
+                </div>
+            </div>
+        </div>
+        <div class="vantagem col-12 col-sm-4 col-lg-4 col-xl-4">
+            <div class="centralizar">
+                <div class="icon">
+                    <img src="img/comoComprar/semJuros.svg" alt="">
+                </div>
+                <div class="texts">
+                    <h3>Até 6x sem juros</h3>
+                    <small>Parcela mínima de R$15,00</small>
+                </div>
             </div>
         </div>
     </div>
